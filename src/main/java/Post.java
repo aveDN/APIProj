c815f2e47449a9fcf596ed923717d26ce8118bee
Dmitriy Necheporenko <j.ave@yandex.ru>
2023-03-13 23:46:33 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean substcription;
+
+    public FormDate birthday;
+    // добавьте другие поля
+}
